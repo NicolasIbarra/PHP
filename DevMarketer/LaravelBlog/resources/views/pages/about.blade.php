7034175119f8b2bbb132b201e3800e8265130747
Nico <nicolasibarra84@gmail.com>
2017-03-04 10:29:29 -0300

    Simplificamos una página creando PARTIALS

--- DevMarketer/LaravelBlog/resources/views/pages/about.blade.php
+++ DevMarketer/LaravelBlog/resources/views/pages/about.blade.php
@@ -1,6 +1,8 @@
 
 @extends('main')
 
+@section('title', '| About')
+
 @section('content')
         <div class="row">
             <div class="col-md-12">
@@ -13,35 +15,7 @@
                     </div>
                 </div>
             </div>
-
-            <div class="row">
-            <div class="col-md-8">
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <hr>
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <hr>
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <hr>
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <hr>
-            </div>
-            <div class="col-md-3 col-md-offset-1">
-                <h3>Side Bar</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </div>
-        </div>
+        <hr>
         </div>
     </div>
 
---
+++ DevMarketer/LaravelBlog/resources/views/partials/_footer.blade.php
@@ -0,0 +1,2 @@
+<hr>
+<p class="text-center">Copyright Nico Pavez 2017</p>
